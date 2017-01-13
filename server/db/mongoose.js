05efa6171a92a3b7941937f7f590729425a5f655
Hugo Aguero <hugo.aguero@softonitg.com>
2017-01-13 13:58:34 -0600

    Fixed database settings

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://ProgrammerHugo:ProgrammerHugo1978.@ds163738.mlab.com:63738/todoapi'|| 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
